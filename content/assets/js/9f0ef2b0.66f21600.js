afa0d5cccc82bf8c77d0d6cceb40fdaf86d5bb3d
Rohit Yadav <rohit.yadav@shapeblue.com>
2023-07-24 16:28:25 +0530

    new blog: ccc2023 CFP and registration
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- content/assets/js/9f0ef2b0.66f21600.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[2308],{8455:e=>{e.exports=JSON.parse('{"permalink":"/blog/page/12","page":12,"postsPerPage":10,"totalPages":13,"totalCount":121,"previousPage":"/blog/page/11","nextPage":"/blog/page/13","blogDescription":"Apache CloudStack Blog","blogTitle":"Blog"}')}}]);
--- content/assets/js/e103c947.2faee537.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[4767],{442:e=>{e.exports=JSON.parse('{"label":"news","permalink":"/blog/tags/news","allTagsPath":"/blog/tags","count":18}')}}]);
