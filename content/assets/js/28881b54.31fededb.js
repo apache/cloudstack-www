efd4ad64e20a7b0529757ac595405051b3eedb2a
Rohit Yadav <rohit.yadav@shapeblue.com>
2023-10-03 17:21:52 +0530

    new blog: ccc agenda revealed
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- content/assets/js/28881b54.31fededb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[9317],{65893:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/news/page/2","page":2,"postsPerPage":10,"totalPages":3,"totalCount":21,"previousPage":"/blog/tags/news","nextPage":"/blog/tags/news/page/3","blogDescription":"Apache CloudStack Blog","blogTitle":"Blog"}')}}]);
