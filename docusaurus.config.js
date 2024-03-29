c840de46df6cd2f60e733a913ed97a2c39f9d26f
Daan Hoogland <daan@onecht.net>
2024-03-29 10:26:25 +0100

    metadata tag in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,12 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      metadata: [
+        { name: 'keywords', content: 'cloud, IaaS, orchestration' },
+        {
+          name: 'description',
+          content: 'Apache CloudStack is open-source software designed to deploy and manage large networks of virtual machines, as a highly available, highly scalable Infrastructure as a Service (laaS) cloud computing platform. CloudStack is used by a number of service providers to offer public cloud services, and by many companies to provide an on-premises (private) cloud offering, or as part of a hybrid cloud solution.'},
+      ],
       navbar: {
         title: '',
         logo: {
