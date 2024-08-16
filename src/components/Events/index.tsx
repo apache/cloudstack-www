f71fb92e1ba9ed14ef00cb081181191a78007e92
Daan Hoogland <daan@onecht.net>
2024-08-16 16:00:09 +0200

    links and whitespace

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -24,22 +24,21 @@ involved with the community.
               <div class="hc-header">Upcoming Events:</div>
               <div class="row">
                 <div class="col-lg-9 pt-3 px-4">
-                  <img src="img/CSEUG 2024.jpg" alt="" class="events-img events-overview-img"/>
+                  <img src="/img/CSEUG 2024.jpg" alt="" class="events-img events-overview-img"/>
                   <p class="title mt-2">CloudStack European User Group 2024</p>
-                  <p class="events-info"><img src="img/pin_icon.png" class="me-2" alt=""/>Frankfurt, Germany</p>
-                  <p class="events-info"><img src="img/calendar_icon.png" class="me-2" alt=""/>Sep 12, 2024</p>
+                  <p class="events-info"><img src="/img/pin_icon.png" class="me-2" alt=""/>Frankfurt, Germany</p>
+                  <p class="events-info"><img src="/img/calendar_icon.png" class="me-2" alt=""/>Sep 12, 2024</p>
                 </div>
                 <div class="col-lg-3 px-4 events-btn-margin d-flex justify-content-center justify-content-lg-end align-items-center">
                   <a href="/blog/cseug-2024" class="btn btn-outline-secondary me-3">Read More</a>
                   <a class="button button--primary" href="https://www.eventbrite.co.uk/e/cloudstack-european-user-group-germany-tickets-879369205967" target="_blank">Register Now</a>
-
                 </div>
               </div>
               <div class="row">
                 <div class="col-lg-9 pt-3 px-4">
                   <div class="events-img events-overview-img" style={{display: "flex",  alignItems: "center"}}>
                     <img src="https://www.apachecon.com/event-images/na2024-wide-light.png" alt="C/C NA 2024"/>
-                  </div>                  
+                  </div>
                   <p class="title mt-2">Community Over Code North America 2024</p>
                   <p class="events-info"><img src="img/pin_icon.png" class="me-2" alt=""/>Denver, United States of America</p>
                   <p class="events-info"><img src="img/calendar_icon.png" class="me-2" alt=""/>October 7-10, 2024</p>
@@ -47,12 +46,11 @@ involved with the community.
                 <div class="col-lg-3 px-4 events-btn-margin d-flex justify-content-center justify-content-lg-end align-items-center">
                   <a href="https://communityovercode.org/" class="btn btn-outline-secondary me-3">Read More</a>
                   <a class="button button--primary" href="https://communityovercode.org/registration/" target="_blank">Register Now</a>
-
                 </div>
               </div>
               <div class="row">
                 <div class="col-lg-9 pt-3 px-4">
-                  <img src="img/CCC-2024.png" alt="" class="events-img events-overview-img"/>
+                  <img src="/img/CCC-2024.png" alt="" class="events-img events-overview-img"/>
                   <p class="title mt-2">CloudStack Collaboration Conference 2024</p>
                   <p class="events-info"><img src="img/pin_icon.png" class="me-2" alt=""/>Madrid, Spain</p>
                   <p class="events-info"><img src="img/calendar_icon.png" class="me-2" alt=""/>November 20-22, 2024</p>
@@ -68,7 +66,7 @@ involved with the community.
               <div class="hc-header">Past Events:</div>
               <div class="row">
                 <div class="col-lg-9 pt-3 px-4">
-                  <img src="img/india 2024.jpeg" alt="" class="events-img events-overview-img"/>
+                  <img src="/img/india 2024.jpeg" alt="" class="events-img events-overview-img"/>
                   <p class="title mt-2">CloudStack India User Group 2024</p>
                   <p class="events-info"><img src="img/pin_icon.png" class="me-2" alt=""/>Hyderabad, India</p>
                   <p class="events-info"><img src="img/calendar_icon.png" class="me-2" alt=""/>February 23, 2024</p>
@@ -80,7 +78,7 @@ involved with the community.
               </div>
               <div class="row">
                 <div class="col-lg-9 pt-3 px-4">
-                  <img src="img/ceph day.png" alt="" class="events-img events-overview-img"/>
+                  <img src="/img/ceph day.png" alt="" class="events-img events-overview-img"/>
                   <p class="title mt-2">Ceph and CloudStack day 2024</p>
                   <p class="events-info"><img src="img/pin_icon.png" class="me-2" alt=""/>Amsterdam, the Netherlands</p>
                   <p class="events-info"><img src="img/calendar_icon.png" class="me-2" alt=""/>February 22, 2024</p>
@@ -91,7 +89,7 @@ involved with the community.
               </div>
               <div class="row">
                 <div class="col-lg-9 pt-3 px-4">
-                  <img src="img/event_img2.png" alt="" class="events-img events-overview-img"/>
+                  <img src="/img/event_img2.png" alt="" class="events-img events-overview-img"/>
                   <p class="title mt-2">CloudStack Collaboration Conference 2023</p>
                   <p class="events-info"><img src="img/pin_icon.png" class="me-2" alt=""/>Paris, Île-de-France, France</p>
                   <p class="events-info"><img src="img/calendar_icon.png" class="me-2" alt=""/>November 23-24, 2023</p>
@@ -103,7 +101,7 @@ involved with the community.
               </div>
               <div class="row">
                 <div class="col-lg-9 pt-3 px-4">
-                  <img src="img/CSEUG 2023.png" alt="" class="events-img events-overview-img"/>
+                  <img src="/img/CSEUG 2023.png" alt="" class="events-img events-overview-img"/>
                   <p class="title mt-2">CloudStack European User Group 2023</p>
                   <p class="events-info"><img src="img/pin_icon.png" class="me-2" alt=""/>London, UK</p>
                   <p class="events-info"><img src="img/calendar_icon.png" class="me-2" alt=""/>May 4, 2023</p>
@@ -116,7 +114,7 @@ involved with the community.
             </div>
 
             <div class="text-center load-more">
-              <a href="blog/tags/roundup" class="btn btn-outline-secondary">See Past Events</a>
+              <a href="/blog/tags/roundup" class="btn btn-outline-secondary">See Past Events</a>
             </div>
 
           </div>
