e0ec13f8b7dd0e1bb349ab8f4d34bbd20b95acbc
Vishesh <vishesh92@gmail.com>
2025-07-15 18:47:03 +0530

    Remove CSIUG 2025 from upcoming events (#342)

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -36,18 +36,6 @@ involved with the community.
               </div>
               <div className="row">
                 <div className="col-lg-9 pt-3 px-4">
-                  <img src="/img/csiug2025.png" alt="" className="events-img events-overview-img"/>
-                  <p className="title mt-2">CloudStack India User Group 2025</p>
-                  <p className="events-info"><img src="img/pin_icon.png" className="me-2" alt=""/>Greater Noida, India</p>
-                  <p className="events-info"><img src="img/calendar_icon.png" className="me-2" alt=""/>July 11th, 2025</p>
-                </div>
-                <div className="col-lg-3 px-4 events-btn-margin d-flex justify-content-center justify-content-lg-end align-items-center">
-                  <a href="https://www.eventbrite.com/e/cloudstack-india-user-group-2025-tickets-1374146161159?aff=oddtdtcreator" className="btn btn-primary me-3">Register</a>
-                  <a href="/blog/csiug-2025" className="btn btn-outline-secondary me-3">Read More</a>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-lg-9 pt-3 px-4">
                   <img src="/img/ccc-2025.jpg" alt="" className="events-img events-overview-img"/>
                   <p className="title mt-2">CloudStack Collaboration Conference 2025</p>
                   <p className="events-info"><img src="img/pin_icon.png" className="me-2" alt=""/>Milan, Italy</p>
